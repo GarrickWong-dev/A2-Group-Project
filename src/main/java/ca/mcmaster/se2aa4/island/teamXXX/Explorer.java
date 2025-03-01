c0f4c6349bf4f784c9f38d65be7390d5d9fb9bdd
GarrickWong-dev <wongg36@mcmaster.ca>
2025-03-01 15:10:28 -0500

    Playing with code, learning how it works

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Explorer.java
@@ -11,6 +11,8 @@ import org.json.JSONTokener;
 public class Explorer implements IExplorerRaid {
 
     private final Logger logger = LogManager.getLogger();
+    private int move = 0;
+    private Integer totalCost = 0;
 
     @Override
     public void initialize(String s) {
@@ -25,11 +27,24 @@ public class Explorer implements IExplorerRaid {
 
     @Override
     public String takeDecision() {
+        this.move++;
         JSONObject decision = new JSONObject();
-        decision.put("action", "stop"); // we stop the exploration immediately
+        if (this.move == 25){
+            decision.put("action", "stop"); // we stop the exploration immediately
+        }else if (this.move % 2 == 1){
+            decision.put("action", "heading");
+            decision.put("parameters", new JSONObject().put("direction", "E"));
+        }else if (this.move % 2 == 0){
+            decision.put("action", "heading");
+            decision.put("parameters", new JSONObject().put("direction", "S"));
+
+        }
+
         logger.info("** Decision: {}",decision.toString());
         return decision.toString();
     }
+
+
 
     @Override
     public void acknowledgeResults(String s) {
@@ -37,6 +52,8 @@ public class Explorer implements IExplorerRaid {
         logger.info("** Response received:\n"+response.toString(2));
         Integer cost = response.getInt("cost");
         logger.info("The cost of the action was {}", cost);
+        totalCost += cost;
+        logger.info("Total cost is {}", totalCost);
         String status = response.getString("status");
         logger.info("The status of the drone is {}", status);
         JSONObject extraInfo = response.getJSONObject("extras");
