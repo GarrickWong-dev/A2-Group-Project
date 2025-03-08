ea4528bf79bcee56e01e1f3cf759fb732edbbf0d
GarrickWong-dev <wongg36@mcmaster.ca>
2025-03-08 14:57:21 -0500

    example for how to use new functionalities

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Explorer.java
@@ -11,8 +11,10 @@ import org.json.JSONTokener;
 public class Explorer implements IExplorerRaid {
 
     private final Logger logger = LogManager.getLogger();
-    private int move = 0;
-    private Integer totalCost = 0;
+    private int move = 0; //Decides what action to do
+    private Integer totalCost = 0; //Nothing right now
+    private CoordinateManager coords = CoordinateManager.getInstance(); // Coordinage Manager
+    private Actions actions = Actions.getInstance(coords); //Actions class
 
     @Override
     public void initialize(String s) {
@@ -27,20 +29,81 @@ public class Explorer implements IExplorerRaid {
 
     @Override
     public String takeDecision() {
-        this.move++;
+        move ++;
         JSONObject decision = new JSONObject();
-        if (this.move == 25){
-            decision.put("action", "stop"); // we stop the exploration immediately
-        }else if (this.move % 2 == 1){
-            decision.put("action", "heading");
-            decision.put("parameters", new JSONObject().put("direction", "E"));
-        }else if (this.move % 2 == 0){
-            decision.put("action", "heading");
-            decision.put("parameters", new JSONObject().put("direction", "S"));
-
+        if (move % 2 == 0){
+            decision.put("action", "scan");
         }
+        if (move == 1){
+        actions.forward(decision); //Move Forward in East Direction
+        logger.info(coords.getX());
+        logger.info(coords.getY());
+        }
+        if (move == 3){
+        actions.forward(decision); //Move forward in east direction
+        logger.info(coords.getX());
+        logger.info(coords.getY());
+        }   
+        if (move == 5){
+        actions.forward(decision); //Move forward in east direction
+        logger.info(coords.getX());
+        logger.info(coords.getY());
+        }
+        if (move == 7){
+        actions.south(decision); //south turn
+        logger.info(coords.getX());
+        logger.info(coords.getY());
+        }
+        if (move == 9){
+        actions.forward(decision); //Forward in south direction
+        logger.info(coords.getX());
+        logger.info(coords.getY());
+        }
+        if (move == 11){
+        actions.forward(decision); //Forward in south direction
+        logger.info(coords.getX());
+        logger.info(coords.getY());
+        }
+        if (move == 13){
+        actions.west(decision); //west turn
+        logger.info(coords.getX());
+        logger.info(coords.getY());
+        }
+        if (move == 15){
+        actions.forward(decision); //Move forward in west direction
+        logger.info(coords.getX());
+        logger.info(coords.getY());
+        }
+        if (move == 17){
+        actions.north(decision); //North turn
+        logger.info(coords.getX());
+        logger.info(coords.getY());
+        
+        }
+        if (move == 19){
+        actions.forward(decision); //Move forward in North direction
+        logger.info(coords.getX());
+        logger.info(coords.getY());
+        
+        }
+        if (move == 21){
+        actions.east(decision); //east turn
+        logger.info(coords.getX());
+        logger.info(coords.getY());
+        
+        }
+        if (move == 23){
+        actions.forward(decision); //Move forward in east direction
+        logger.info(coords.getX());
+        logger.info(coords.getY());
+        
+        }   
 
-        logger.info("** Decision: {}",decision.toString());
+
+        if (move == 25){
+        decision.put("action", "stop");
+        } // we stop the exploration immediately
+        //logger.info("** Decision: {}",decision.toString());
         return decision.toString();
     }
 
@@ -49,20 +112,23 @@ public class Explorer implements IExplorerRaid {
     @Override
     public void acknowledgeResults(String s) {
         JSONObject response = new JSONObject(new JSONTokener(new StringReader(s)));
-        logger.info("** Response received:\n"+response.toString(2));
+        //logger.info("** Response received:\n"+response.toString(2));
         Integer cost = response.getInt("cost");
-        logger.info("The cost of the action was {}", cost);
+        //logger.info("The cost of the action was {}", cost);
         totalCost += cost;
-        logger.info("Total cost is {}", totalCost);
+        //logger.info("Total cost is {}", totalCost);
         String status = response.getString("status");
-        logger.info("The status of the drone is {}", status);
+        //logger.info("The status of the drone is {}", status);
         JSONObject extraInfo = response.getJSONObject("extras");
-        logger.info("Additional information received: {}", extraInfo);
+        //logger.info("Additional information received: {}", extraInfo);
     }
 
     @Override
     public String deliverFinalReport() {
         return "no creek found";
     }
+
+
+
 
 }
