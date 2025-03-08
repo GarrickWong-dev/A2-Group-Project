901eba5ae306d6ec935556461ab7bf0afcc8694f
GarrickWong-dev <wongg36@mcmaster.ca>
2025-03-08 14:41:12 -0500

    Classes that track XY coordinates and related data. Issue #21

---
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/XYCoordinates.java
@@ -0,0 +1,26 @@
+package ca.mcmaster.se2aa4.island.teamXXX;
+
+public class XYCoordinates{
+    private int X;
+    private int Y;
+    public XYCoordinates(int X, int Y){
+        this.X = X;
+        this.Y = Y;
+    }
+
+    public int getX(){
+        return this.X;
+    }
+
+    public int getY(){
+        return this.Y;
+    }
+
+    public void setX(int X){
+        this.X = X;
+    }
+
+    public void setY(int Y){
+        this.Y = Y;
+    }
+}
